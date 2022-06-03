1fdfe94d5db1cd697a3620f622a84df10c89223a
zyt0328 <71568759+zyt0328@users.noreply.github.com>
2022-06-03 14:08:05 -0700

    Update ReverseString.java

--- ReverseString.java
+++ ReverseString.java
@@ -14,3 +14,17 @@ class Solution {
         }
     }
 }
+
+class Solution {
+    public void reverseString(char[] s) {
+        int r = s.length-1;
+        int l = 0;
+        while(l<r){
+            char temp = s[l];
+            s[l] = s[r];
+            s[r] = temp;
+            l++;
+            r--;
+        }
+    }
+}
